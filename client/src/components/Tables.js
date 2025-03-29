68e9c677f5ef27a010f2b33bb4eca7f7ea6ad948
Chaitanya0311 <102011709+Chaitanya0311@users.noreply.github.com>
2025-03-29 15:49:02 +0530

    Update Tables.js

--- client/src/components/Tables.js
+++ client/src/components/Tables.js
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
@@ -17,7 +18,7 @@ const Tables = ({ onTableSelect, selectedTableId }) => {
 
     const fetchTables = async () => {
         try {
-            const response = await axios.get('http://localhost:5000/api/tables', {
+            const response = await axios.get('http://3.109.143.125/api/tables', {
                 headers: {
                     'Cache-Control': 'no-cache',
                     'Pragma': 'no-cache'
