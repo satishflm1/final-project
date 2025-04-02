cc160da23530ca86102b1cc7bf097f973a370be2
Chaitanya0311 <102011709+Chaitanya0311@users.noreply.github.com>
2025-04-02 19:17:47 +0530

    Update1 Tables.js

--- client/src/components/Tables.js
+++ client/src/components/Tables.js
@@ -1,7 +1,8 @@
 
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-const API_BASE_URL = "http://3.110.210.194/api/"
+const API_BASE_URL = "http://3.110.210.194/api/";
+
 const Tables = ({ onTableSelect, selectedTableId }) => {
     const [tables, setTables] = useState([]);
     const [loading, setLoading] = useState(true);
