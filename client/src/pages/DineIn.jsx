2eadc3ac54877919b7420e7a3d6a22c9787aadaa
Chaitanya0311 <102011709+Chaitanya0311@users.noreply.github.com>
2025-04-02 19:38:26 +0530

    Update1 DineIn.jsx

--- client/src/pages/DineIn.jsx
+++ client/src/pages/DineIn.jsx
@@ -5,7 +5,8 @@ import { QRCodeSVG } from 'qrcode.react';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-const API_BASE_URL = 'http://3.109.143.125';
+
+const API_BASE_URL = 'http://3.110.210.194';
 const FRONTEND_BASE_URL = 'http://3.109.143.125';
 
 const DineIn = () => {
