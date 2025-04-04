a2923aee6263962eec75c13b2c63e48b0e52a7b2
satishflm1 <satish052000@gmail.com>
2025-04-04 11:22:16 +0530

    Update TakeAway.jsx

--- client/src/pages/TakeAway.jsx
+++ client/src/pages/TakeAway.jsx
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { QRCodeSVG } from 'qrcode.react';
 
-const API_BASE_URL = 'http://3.7.47.120';
-const FRONTEND_BASE_URL = 'http://3.7.47.120:8080';
+const API_BASE_URL = 'http://15.207.252.191';
+const FRONTEND_BASE_URL = 'http://15.207.252.191:3000';
 
 const TakeAway = () => {
   const [categories, setCategories] = useState([]);
