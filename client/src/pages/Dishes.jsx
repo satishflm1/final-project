8d6801e68f2a0024ac6c08f54562bc9b3d4d8329
satishflm1 <satish052000@gmail.com>
2025-04-04 12:24:01 +0530

    Update Dishes.jsx

--- client/src/pages/Dishes.jsx
+++ client/src/pages/Dishes.jsx
@@ -1,7 +1,7 @@
 
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-const API_BASE_URL = 'http://15.207.252.191';
+const API_BASE_URL = 'http://15.207.252.191:5000';
 const Dishes = () => {
   const [dishes, setDishes] = useState([]);
   const [categories, setCategories] = useState([]);
