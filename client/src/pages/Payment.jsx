6f3f86713e1328a09164f4e21e78c449fe15b45b
satishflm1 <satish052000@gmail.com>
2025-04-04 12:25:47 +0530

    Update Payment.jsx

--- client/src/pages/Payment.jsx
+++ client/src/pages/Payment.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useCallback } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
-const API_BASE_URL = 'http://15.207.252.191';
+const API_BASE_URL = 'http://15.207.252.191:5000';
 
 const Payment = () => {
   const { orderId } = useParams();
