4c1fb2296788aa0dfd22a1a9a4cdfa2736ad5678
satishflm1 <satish052000@gmail.com>
2025-04-04 11:14:46 +0530

    Update Categories.jsx

--- client/src/pages/Categories.jsx
+++ client/src/pages/Categories.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-const API_BASE_URL = 'http://3.7.47.120';
+const API_BASE_URL = 'http://15.207.252.191';
 const CategoryPage = () => {
   const [categories, setCategories] = useState([]);
   const [newCategory, setNewCategory] = useState({ name: '' });
