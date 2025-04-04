d98322c0025a7763efaab825a04398c103dbf07d
satishflm1 <satish052000@gmail.com>
2025-04-04 12:34:04 +0530

    Update Cart.jsx

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -63,7 +63,7 @@ const Cart = forwardRef(({ tableId, onOrderComplete }, ref) => {
     const fetchTaxes = async () => {
         try {
             setLoading(true);
-            const response = await axios.get('http://3.109.143.125/api/taxes');
+            const response = await axios.get('http://15.207.252.191:500/api/taxes');
             setTaxes(response.data);
             setError(null);
         } catch (err) {
@@ -142,11 +142,11 @@ const Cart = forwardRef(({ tableId, onOrderComplete }, ref) => {
 
             console.log('Cart: Sending order data:', orderData);
 
-            const response = await axios.post('http://3.109.143.125/api/orders', orderData);
+            const response = await axios.post('http://15.207.252.191:5000/api/orders', orderData);
             console.log('Cart: Order created successfully:', response.data);
 
             // Generate bill
-            const billResponse = await axios.get(`http://3.109.143.125/api/orders/${response.data.id}/bill`);
+            const billResponse = await axios.get(`http://15.207.252.191:5000/api/orders/${response.data.id}/bill`);
             setBillData(billResponse.data);
             setShowBill(true);
 
@@ -275,7 +275,7 @@ const Cart = forwardRef(({ tableId, onOrderComplete }, ref) => {
             generatePDF();
             
             // If PDF generation was successful, proceed with closing the bill
-            await axios.patch(`http://3.109.143.125/api/tables/${tableId}/status`, {
+            await axios.patch(`http://15.207.252.191:5000/api/tables/${tableId}/status`, {
                 status: 'available'
             });
             
