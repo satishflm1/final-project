6a8be2c196c23102ecdefb684b65ff75c2587d99
satishflm1 <satish052000@gmail.com>
2025-04-04 11:21:35 +0530

    Update Tables.jsx

--- client/src/pages/Tables.jsx
+++ client/src/pages/Tables.jsx
@@ -6,9 +6,8 @@ import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Modal from '../components/Modal';
 
-const API_BASE_URL = 'http://3.7.47.120';
-const FRONTEND_BASE_URL = 'http://3.7.47.120:8080';
-
+const API_BASE_URL = 'http://15.207.252.191';
+const FRONTEND_BASE_URL = 'http://15.207.252.191:3000';
 const TableManagementPage = () => {
   const [tables, setTables] = useState([]);
   const [showAddModal, setShowAddModal] = useState(false);
