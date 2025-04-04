24a96e6b7d1d81c3e7f5f94aa1d869e487553735
satishflm1 <satish052000@gmail.com>
2025-04-04 12:28:17 +0530

    Update Taxes.jsx

--- client/src/pages/Taxes.jsx
+++ client/src/pages/Taxes.jsx
@@ -1,7 +1,7 @@
 
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-const API_BASE_URL = 'http://15.207.252.191';
+const API_BASE_URL = 'http://15.207.252.191:5000';
 const TaxPage = () => {
   const [taxes, setTaxes] = useState([]);
   const [newTax, setNewTax] = useState({ tax_name: '', percentage: '' });
